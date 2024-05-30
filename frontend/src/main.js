7fc774b963657417da729c7c07d091fe3b7a1a7d
KeineWieDu <578586993@qq.com>
2024-05-30 10:38:00 +0800

    feat: 非常简略的地图初步完成

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
+const prefixUrl = "http://127.0.0.1:4523/m1/4554522-0-default"
 
 app.use(createPinia())
 
 app.mount('#app')
+
+export {prefixUrl}
