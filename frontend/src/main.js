e7b9125cdec960115fbd966cd95d398d095e56f2
KeineWieDu <578586993@qq.com>
2024-05-31 11:10:42 +0800

    feat: 调用本地前端

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
-const prefixUrl = "http://127.0.0.1:4523/m1/4554522-0-default"
+const prefixUrl = "http://127.0.0.1:8000"
 
 app.use(createPinia())
 
