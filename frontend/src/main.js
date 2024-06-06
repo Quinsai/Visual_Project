3e61a1d7c90c27dc3ee14ce58800ff8a382730e0
KeineWieDu <578586993@qq.com>
2024-06-06 11:39:43 +0800

    feat: 引入acro ui

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 
 const app = createApp(App)
 const prefixUrl = "http://127.0.0.1:8000"
 
 app.use(createPinia())
+app.use(ArcoVue)
 
 app.mount('#app')
 
